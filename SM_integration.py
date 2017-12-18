4333bebc73b68005b8e7b7818fbf71934cce4cad
dizcza <dizcza@gmail.com>
2017-12-18 22:19:30 +0200

    first run refactored

--- SM_integration.py
+++ SM_integration.py
@@ -7,78 +7,66 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from sdr_drom_flaot import NumberEncoder
+from sdr_drom_flaot import ScalarEncoder
 
 
-class World():
+class World(object):
     def __init__(self):
-        self.visual_field_size = (100, 100)
-        self.visual_field = np.zeros(self.visual_field_size)
+        self.size_pixels = (100, 100)
+        self.image = np.zeros(self.size_pixels, dtype=np.uint8)
 
-        self.agent = 0
-
-    def add_visual_data(self, data, position):
-        # data is a two dimensional array = image
+    def add_image(self, new_image, position):
+        # new_image is a two dimensional array
         x, y = position
-        x_m, y_m = data.shape
-        self.visual_field[x:x+x_m, y:y+y_m] += data
+        im_h, im_w = new_image.shape[:2]
+        self.image[y: y+im_h, x: x+im_w] = new_image
 
 
-
-class Agent():
+class Agent(object):
     def __init__(self):
-        self.input_size_angle = (30, 30)  # in degrees
+        self.cortex = Cortex()
+        self.receptive_field_angle_xy = (30, 30)  # in degrees
         # x,y,z. XY plane of an image.
         # do not change z for now, it adjusted with self.visual_field_angle to give 28x28 patch of visual input
 
         self.position = np.array([10, 10, 25])  # relative to the world
-        self.input_size_pixel = (2 * self.position[2] * np.tan(np.deg2rad(self.input_size_angle))).astype(int)
+        self.receptive_field_pixels = (2 * self.position[2] * np.tan(np.deg2rad(self.receptive_field_angle_xy))).astype(int)
 
     def sense_data(self, world):
         x, y, z = self.position
-        x_m, y_m = self.input_size_pixel
-        return world.visual_field[x:x+x_m, y:y+y_m]
+        fov_w, fov_h = self.receptive_field_pixels
+        return world.image[y: y+fov_h, x: x+fov_w]
 
-class Area:
+
+class Area(object):
     def __init__(self):
-        self.layer = {}
+        self.layers = {}
 
     def add_layer(self, name, layer):
-        self.layer[name] = layer
+        self.layers[name] = layer
 
 
-class Layer:
+class Layer(object):
     def __init__(self, shape):
-        self.cells = np.zeros(shape, dtype=int)
-        self.shape = shape  # in case of two dimensions
-        self.size = np.prod(self.shape)
-
+        self.cells = np.zeros(shape, dtype=np.int32)
+        self.size = np.prod(shape)
         self.input_layers = []
         self.weights = []
+        self.sparsity = 0.1
+        
         # association memory params
         self.cluster_size = 2
         self.clusters = 'tbd'
 
-    def connect(self, with_layer):
-        self.input_layers.append(with_layer)
-        self.weights.append(np.random.rand(self.size, with_layer.size))
+    def connect_input(self, new_layer):
+        self.input_layers.append(new_layer)
+        self.weights.append(np.random.rand(self.size, new_layer.size))
 
     def linear_update(self):
-        for i, layer in enumerate(self.input_layers):
-            self.cells = self.cells + np.dot(self.weights[i], layer.cells.flatten())
-
-        self.cells = self.kWTA(self.cells, 0.1)
-
-    def cluster_activation(self, clusters, input):
-        dst = clusters[:, np.nonzero(input)[0]]
-        # find counts of input*distance to see if there is a cluster
-        data = [np.unique(dt[np.nonzero(dt)[0], np.nonzero(dt)[1]], return_counts=True) for dt in dst]
-        # find max in each count to see if there is cluster (2 if there is a cluster)
-        data2 = np.array([np.max(counts) if counts.size else 0 for counts in np.array(data)[:, 1]])
-        # cells = data2 >= np.sort(data2)[-int(self.size*self.activation)]
-        cells = (data2 == 2).astype(int)
-        # print np.count_nonzero(cells)
-        return cells
+        signal = np.zeros(self.cells.shape, dtype=np.float32)
+        for layer_id, layer in enumerate(self.input_layers):
+            signal += np.dot(self.weights[layer_id], layer.cells.flatten())
+        self.cells = self.k_winners_take_all(signal, self.sparsity)
 
     def associate(self, input_layers=[]):
         return 0
@@ -109,13 +97,34 @@ class Layer:
                     else:
                         print 'full'
 
-    def kWTA(self, vector, k=0.1):
-        # k - percent of active neurons to leave
-        vector[vector.argsort()[:-int(vector.size * k)]] = 0
-        vector[vector.argsort()[-int(vector.size * k):]] = 1
-        return vector.astype(int)
+    def k_winners_take_all(self, activations, sparsity):
+        n_active = max(int(sparsity * len(activations)), 1)
+        winners = np.argsort(activations)[-n_active:]
+        sdr = np.zeros(activations.shape, dtype=self.cells.dtype)
+        sdr[winners] = 1
+        return sdr
 
-class Cortex:
+
+class LocationLayer(Layer):
+    def __init__(self, max_amplitude):
+        super(LocationLayer, self).__init__(shape=(28, 28))
+        self.max_amplitude = float(max_amplitude)
+
+    @staticmethod
+    def get_phase(vector):
+        x, y = vector
+        phase = np.arctan2(y, x)
+        # transform [-pi, pi] --> [0, 1]
+        phase = (phase / np.pi + 1.) / 2.
+        return phase
+
+    def get_amplitude(self, vector):
+        ampl = np.linalg.norm(vector)
+        ampl = ampl / self.max_amplitude
+        return ampl
+
+
+class Cortex(object):
     def __init__(self):
         self.V1 = Area()
 
@@ -124,63 +133,61 @@ class Cortex:
         self.V1.add_layer('motor_direction', Layer(shape=100))
         self.V1.add_layer('motor_amplitude', Layer(shape=100))
 
-        self.V1.add_layer('saliency', Layer(shape=(28, 28)))
+        self.V1.add_layer('saliency', LocationLayer(max_amplitude=30))
         self.retina = Layer(shape=(28, 28))
 
-        self.number_encoder = NumberEncoder(sdr_size=100)
+        self.scalar_encoder = ScalarEncoder(size=100)
 
-        self.V1.layer['L4'].connect(self.retina)
+        self.V1.layers['L4'].connect_input(self.retina)
 
-        self.V1.layer['L23'].connect(self.V1.layer['L4'])
-        self.V1.layer['L23'].connect(self.V1.layer['motor_direction'])
-        self.V1.layer['L23'].connect(self.V1.layer['motor_amplitude'])
+        self.V1.layers['L23'].connect_input(self.V1.layers['L4'])
+        self.V1.layers['L23'].connect_input(self.V1.layers['motor_direction'])
+        self.V1.layers['L23'].connect_input(self.V1.layers['motor_amplitude'])
 
-
-    def salience_map(self, img):
-        # result = np.zeros((img.shape[0], img.shape[1]), dtype='float64')
-        result = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1,0,ksize=3)) + np.abs(cv2.Sobel(img, cv2.CV_64F, 0, 1,ksize=3))
-        if np.max(result):
-            result = result/np.max(result)
+    def saliency_map(self, image):
+        image = image.astype(np.uint8)
+        result = np.absolute(cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)) + np.abs(cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3))
+        result = result / (np.max(result) + 1e-7)
         thresh = result > 0.5
         return result * thresh
 
-    def get_vector_to_saliency(self, s_map):
-        x_c, y_c = int(s_map.shape[0]/2), int(s_map.shape[1]/2)
-
-        mask = np.ones(s_map.shape, dtype=int)
-        mask[x_c-5:x_c+5, y_c-5:y_c+5] = 0
-        s_map *= mask    # move somewhere else not to current place
-
-        x, y = np.unravel_index(s_map.argmax(), s_map.shape)
-        plt.imshow(s_map)
+    def get_vector_from_saliency(self, saliency_map):
+        x_c, y_c = int(saliency_map.shape[0] / 2), int(saliency_map.shape[1] / 2)
+        saliency_map[x_c - 5:x_c + 5, y_c - 5:y_c + 5] = 0
+        x, y = np.unravel_index(saliency_map.argmax(), saliency_map.shape)
+        plt.imshow(saliency_map)
         plt.show()
         print x, y
         return x - x_c, y - y_c
 
-    def get_phase_amp(self, vector):
+    @staticmethod
+    def get_phase_amplitude(vector):
         x, y = vector
-        return np.rad2deg(np.arctan(float(x)/y)), np.sqrt(x ** 2 + y ** 2)
+        phase = np.arctan2(y, x)
+        phase = phase / np.pi + 1
+        ampl = np.linalg.norm(vector)
+        return phase, ampl
 
     def saccade(self):
-        self.V1.layer['saliency'].cells = self.salience_map(self.retina.cells)
-        vector = self.get_vector_to_saliency(self.V1.layer['saliency'].cells)
+        saliency_map = self.saliency_map(self.retina.cells)
+        vector = self.get_vector_from_saliency(saliency_map)
 
         # this is one of the output of the brain
         poppy.position[:2] += vector  # rewrite somehow to exclude explicit name of the agent
         self.retina.cells = poppy.sense_data(flat_mnist_world)
 
-        angle, amplitude = self.get_phase_amp(vector)
-        h, w = 30, 30
-        max_amplitude = np.sqrt(h ** 2 + w ** 2)
-        max_angle = 180
-        self.V1.layer['motor_direction'] = self.number_encoder.encode(angle/max_angle)
-        self.V1.layer['motor_amplitude'] = self.number_encoder.encode(amplitude/max_amplitude)
+        phase = self.V1.layers['saliency'].get_phase(vector)
+        amplitude = self.V1.layers['saliency'].get_amplitude(vector)
+        self.V1.layers['motor_direction'] = self.scalar_encoder.encode(phase)
+        self.V1.layers['motor_amplitude'] = self.scalar_encoder.encode(amplitude)
 
     def compute(self):
-        self.V1.layer['L4'].linear_update()
-        self.V1.layer['L23'].linear_update()
+        self.V1.layers['L4'].linear_update()
+        self.V1.layers['L23'].linear_update()
 
-        self.V1.layer['L23'].associate([self.V1.layer['L4'], self.V1.layer['motor_amplitude'], self.V1.layer['motor_direction']])
+        # todo: associate
+        # self.V1.layers['L23'].associate([self.V1.layers['L4'], self.V1.layers['motor_amplitude'], self.V1.layers['motor_direction']])
+
         self.saccade()
 
 
@@ -190,25 +197,16 @@ if __name__ == '__main__':
     load_number = 100
     images, labels = load_mnist.load_images(images_number=load_number)
 
-    images = images.astype(int)
-    # images = images.reshape(load_number, -1)
-    # images /= np.max(images, axis=1)[:, None]
-    # images[images > 0.1] = 1
-    # images[images < 0.1] = 0
-
-
     flat_mnist_world = World()
-    flat_mnist_world.add_visual_data(images[0], position=(10, 10))
+    flat_mnist_world.add_image(images[0], position=(10, 10))
 
     poppy = Agent()
-    poppy.brain = Cortex()
-
 
     poppy_init_position = np.copy(poppy.position)
-    poppy.brain.retina.cells = poppy.sense_data(flat_mnist_world)
+    poppy.cortex.retina.cells = poppy.sense_data(flat_mnist_world)
 
     for i in range(7):
-        poppy.brain.compute()
+        poppy.cortex.compute()
 
-        # plt.imshow(poppy.brain.salience_map(poppy.sense_data(flat_mnist_world)))
-        # plt.show()
+        plt.imshow(poppy.cortex.saliency_map(poppy.sense_data(flat_mnist_world)))
+        plt.show()
--- sdr_drom_flaot.py
+++ sdr_drom_flaot.py
@@ -1,51 +1,63 @@
 import numpy as np
 import os
-import pickle
 
 
-class NumberEncoder(object):
-    def __init__(self, sdr_size=100, sparsity=0.1, discretization=100, similarity=0.8):
-        self.sdr_size = sdr_size
+class ScalarEncoder(object):
+    def __init__(self, size=100, sparsity=0.1, bins=100, similarity=0.8):
+        """
+        Sparse Distributed Representation scalar encoder.
+        :param size: sdr output vector dimensionality
+        :param sparsity: sdr output vector sparsity
+        :param bins: number of bins (1/resolution)
+        :param similarity: how similar two adjacent encodings (bins)
+        """
+        self.size = size
         self.sparsity = sparsity
-        self.discretization = discretization # shows how many sdr should be
-        self.similarity = similarity # shows how similar two adjacent encodings
+        self.bins = bins
+        self.similarity = similarity
 
-        self.datafile = 'data/number_data_num_size_'+str(sdr_size) + '_' +str(discretization)+'_'+str(sparsity)+'.p'
-        if not os.path.exists('data/'):
-            os.makedirs('data/')
-        try:
-            with open(self.datafile) as file:
-                data = pickle.load(file)
-                self.sdr = data
-                print 'here'
-        except:
+        data_dir = "data"
+        self.data_path = "size={}_sparse={}_bins={}_similar={}.npy".format(size, sparsity, bins, similarity)
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        self.data_path = os.path.join(data_dir, self.data_path)
+        if not os.path.exists(self.data_path):
             self.generate_sdr()
-            with open(self.datafile, 'wb') as file:
-                pickle.dump(self.sdr, file)
+        self.sdr = np.load(self.data_path)
 
     def generate_sdr(self):
-        self.sdr = np.zeros((self.discretization, self.sdr_size), dtype=bool)
-        self.sdr[0][np.random.choice(self.sdr_size, size=int(self.sdr_size * self.sparsity), replace=False)] = 1
-        for i in range(1, self.discretization):
-            old = np.random.choice(np.nonzero(self.sdr[i-1])[0], size=int(self.sdr_size * self.sparsity * self.similarity), replace=False)
-            fresh_inds = np.delete(np.arange(self.sdr_size), np.nonzero(self.sdr[i-1])[0])
-            new = np.random.choice(fresh_inds, size=int(self.sdr_size * self.sparsity * (1 - self.similarity)), replace=False)
-            self.sdr[i][old] = 1
-            self.sdr[i][new] = 1
+        self.sdr = np.zeros((self.bins, self.size), dtype=bool)
+        n_active_total = max(int(self.size * self.sparsity), 1)
+        n_active_stay = int(n_active_total * self.similarity)
+        n_active_new = n_active_total - n_active_stay
+        mask_active = np.random.choice(self.size, size=n_active_total, replace=False)
+        self.sdr[0][mask_active] = 1
+        dim_arange = np.arange(self.size)
+        for bin_id in range(1, self.bins):
+            active_prev = np.nonzero(self.sdr[bin_id - 1])[0]
+            active_stay = np.random.choice(active_prev, size=n_active_stay, replace=False)
+            non_active = np.delete(dim_arange, active_prev)
+            active_new = np.random.choice(non_active, size=n_active_new, replace=False)
+            self.sdr[bin_id][active_stay] = 1
+            self.sdr[bin_id][active_new] = 1
+        for sdr_bin in self.sdr:
+            assert len(np.nonzero(sdr_bin)[0]) == n_active_total
+        np.save(self.data_path, self.sdr)
 
-    def encode(self, number):
-        # number should be in range [0, 1)
-        return self.sdr[int(np.rint(number*self.discretization))]
+    def encode(self, scalar):
+        """
+        :param scalar: float value in range [0, 1) 
+        :return: sparse distributed representation vector of `scalar`
+        """
+        bin_active = int(scalar * self.bins)
+        return self.sdr[bin_active]
 
 
 if __name__ == '__main__':
-    num_encoder = NumberEncoder()
-
-    sdr1 = num_encoder.encode(0.47)
-    sdr2 = num_encoder.encode(0.5)
-    print sdr1.astype(int)
-    print np.count_nonzero(sdr1*sdr2)
-
-
-
-
+    encoder = ScalarEncoder()
+    sdr1 = encoder.encode(0.47)
+    sdr2 = encoder.encode(0.5)
+    overlap = np.count_nonzero(sdr1*sdr2)
+    overlap_expected = int(encoder.bins * encoder.sparsity * encoder.similarity)
+    print(sdr1.astype(int))
+    print("Similarity overlap={} (expected={})".format(overlap, overlap_expected))
